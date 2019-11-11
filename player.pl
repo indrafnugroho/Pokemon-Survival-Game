e7251ca7be55572f8ab8736591c3c6d7ae55eb07
dwianiyulia <dwianiyulia@gmail.com>
2019-11-11 11:09:51 +0700

    player

--- player.pl
+++ player.pl
@@ -1,9 +1,45 @@
-:- dynamic(inventory/1).
-:- dynamic(max_inventory/1).
+/* Pemain */
+jml_inventory(0).
+max_inventory(6).
+posisiPlayer(1,1).
+
+/* Inventory */
+add_inv([],X,[X]).
+add_inv([H|T],H,[H|T]).
+add_inv([Z|T1],X,[Z|T2]):- add_inv(T1,X,T2),!.
+
+del_inv([X],X,[]).
+del_inv([H|T],H,[[]|T]).
+del_inv([Z|T1],X,[Z|T2]) :- del_inv(T1,X,T2),!.
+
+catch(X) :-
+    pokemon(Y,X),
+    inventory(X),
+    jml_inventory(N).
+    N1 is N+1,
+    retract(jml_inventory(N)),
+    asserta(jml_inventory(N1)),
+    max_inventory(N1),!,
+    add_inv(A,X,B).
 
 
-/* max_inventory */
-    max_inventory(6).
+throw(X) :- pokemon(Y,X),
+            inventory(X),
+            jml_inventory(N),
+            N1 is N-1,
+            retract(jml_inventory(N)),
+            asserta(jml_inventory(N1)).
+            del_inv(A,X,B).
 
-/*inventori(X): berisi jumlah inventori*/
-    inventori(X):-
+/* gym center */
+heal(X) :- pokemon(Y,X),
+            inventory(X),
+            posisiPlayer(A,B),
+            gym(A,B),
+            curr_health(X,H),
+            health(X,HH),
+            retract(curr_health(X,H)),
+            asserta(curr_health(X,HH)).
+
+/* fail state */
+fail(X) :- jml_inventory(0),!.
