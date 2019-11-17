d037742bd83e66cc5194d5d2f628de72b6c54f45
dwianiyulia <dwianiyulia@gmail.com>
2019-11-17 18:13:01 +0700

    capture + drop

--- player.pl
+++ player.pl
@@ -1,27 +1,22 @@
 :- include('variables.pl').
+:- dynamic(listPoke/1).
+:- dynamic(idAv/1).
 
+idAv([1,2,3,4,5,6]).
 
 /* Pemain */
 /* Kondisi awal pemain */
-inventory(snivy).
-posisiPlayer(1,2).
-posisiPokemon(1,1,2).
-jml_inventory(1).
-curr_health(snivy,10).
-curr_health(victini,100).
-curr_health(virizion,100).
-
-
 
 /* Inventory */
-/* add_inv(X,Y,Z) : menambahkan pokemon Y ke dalam list inventory X */
-add_inv([],X,[X]).
-add_inv([Z|T1],X,[Z|T2]):- add_inv(T1,X,T2),!.
+/* add_id(X,Y,Z) : menambahkan pokemon Y ke dalam list inventory X */
+add_id([],X,[X]).
+add_id([Z|T1],X,[Z|T2]):- add_id(T1,X,T2),!.
+
 
 /* del_inv(X,Y,Z) : menghapus pokemon Y dari inventory X */
-del_inv([H|T],H, T).
-del_inv([H, H2|T],X, [H|T2]) :-
-    del_inv([H2|T], X, T2),!.
+del_id([H|T],H, T).
+del_id([H, H2|T],X, [H|T2]) :-
+    del_id([H2|T], X, T2),!.
 
 /* idx_inv(IN,X,IDX) : mengembalikan nilai indeks IDX elemen X dari inventori IN */
 idx_inv([H|T], H, 1).
@@ -29,45 +24,134 @@ idx_inv([H|T], X, I) :-
     idx_inv(T, X, Z),
     I is (Z+1),!.
 
+/* isExist(X,Y) : Y ada di dalam List X */
+isExist([X],X).
+isExist([H|T],H).
+isExist([H|T],X) :- isExist(T,X),!.
 
-/* catch(X) : Menangkap pokemon X yang sudah dikalahkan pada suatu lokasi tertentu dan memasukannya ke inventory I sehingga jumlah inventory bertambah 1 */
+/* capture(X) : Menangkap pokemon X yang sudah dikalahkan pada suatu lokasi tertentu dan memasukannya ke inventory I sehingga jumlah inventory bertambah 1 */
 capture(X) :-
     pokemon(Y,X),
     posisiPlayer(A,B),
     posisiPokemon(Y,A,B),
+    idAv(List),
+    isExist(List,1),
+    asserta(no_inventory(1,X)),
+    del_id(List,1,Z),
+    retract(idAv(List)),
+    asserta(idAv(Z)),
     jml_inventory(N),
     N1 is N+1,
     N1 < 7,
-    asserta(inventory(X)),
     retract(jml_inventory(N)),
     asserta(jml_inventory(N1)),
-    update_inv,!.
+    asserta(inventory(X)),!.
 
+capture(X) :-
+    pokemon(Y,X),
+    posisiPlayer(A,B),
+    posisiPokemon(Y,A,B),
+    idAv(List),
+    isExist(List,2),
+    asserta(no_inventory(2,X)),
+    del_id(List,2,Z),
+    retract(idAv(List)),
+    asserta(idAv(Z)),
+    jml_inventory(N),
+    N1 is N+1,
+    N1 < 7,
+    retract(jml_inventory(N)),
+    asserta(jml_inventory(N1)),
+    asserta(inventory(X)),!.
+
+capture(X) :-
+    pokemon(Y,X),
+    posisiPlayer(A,B),
+    posisiPokemon(Y,A,B),
+    idAv(List),
+    isExist(List,3),
+    asserta(no_inventory(3,X)),
+    del_id(List,3,Z),
+    retract(idAv(List)),
+    asserta(idAv(Z)),
+    jml_inventory(N),
+    N1 is N+1,
+    N1 < 7,
+    retract(jml_inventory(N)),
+    asserta(jml_inventory(N1)),
+    asserta(inventory(X)),!.
+
+capture(X) :-
+    pokemon(Y,X),
+    posisiPlayer(A,B),
+    posisiPokemon(Y,A,B),
+    idAv(List),
+    isExist(List,4),
+    asserta(no_inventory(4,X)),
+    del_id(List,4,Z),
+    retract(idAv(List)),
+    asserta(idAv(Z)),
+    jml_inventory(N),
+    N1 is N+1,
+    N1 < 7,
+    retract(jml_inventory(N)),
+    asserta(jml_inventory(N1)),
+    asserta(inventory(X)),!.
+
+capture(X) :-
+    pokemon(Y,X),
+    posisiPlayer(A,B),
+    posisiPokemon(Y,A,B),
+    idAv(List),
+    isExist(List,5),
+    asserta(no_inventory(5,X)),
+    del_id(List,5,Z),
+    retract(idAv(List)),
+    asserta(idAv(Z)),
+    jml_inventory(N),
+    N1 is N+1,
+    N1 < 7,
+    retract(jml_inventory(N)),
+    asserta(jml_inventory(N1)),
+    asserta(inventory(X)),!.
+
+capture(X) :-
+    pokemon(Y,X),
+    posisiPlayer(A,B),
+    posisiPokemon(Y,A,B),
+    idAv(List),
+    isExist(List,6),
+    asserta(no_inventory(6,X)),
+    del_id(List,6,Z),
+    retract(idAv(List)),
+    asserta(idAv(Z)),
+    jml_inventory(N),
+    N1 is N+1,
+    N1 < 7,
+    retract(jml_inventory(N)),
+    asserta(jml_inventory(N1)),
+    asserta(inventory(X)),!.
 
 /* drop(X) : Menghapus pokemon X dari inventory */
-drop(X) :- 
+drop(M) :- 
+    no_inventory(M,X),
     pokemon(Y,X),
     inventory(X),
-    retract(inventory(X)),
+    idAv(List),
+    add_id(List,M,List2),
+    retract(idAv(List)),
+    asserta(idAv(List2)),
     jml_inventory(N),
     N1 is N-1,
     retract(jml_inventory(N)),
     asserta(jml_inventory(N1)),
-    del_inv(I,X,I2),
-    update_inv,!.
-
-/* update_inv : mengupdate list inventory */
-update_inv :-
-    inventory(X),
-    add_inv([],X,List2).
-    idx_inv(List2,X,N2),
-    asserta(no_inventory(N2,X)),!.
+    retract(no_inventory(M,X)),
+    retract(inventory(X)),!.
 
 /* choose(X) : Pokemon X dipilih dari inventory untuk battle */
 pick(N) :- 
     no_inventory(N,X),
     pokemon(Y,X),!.
-    /* buat attack (?) */
 
 /* gym center */
 /* healX(X) : Meningkatkan health pokemon X menjadi maksimal seperti semula */
