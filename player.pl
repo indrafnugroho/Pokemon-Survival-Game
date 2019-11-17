284b0412cf0739a397f6ab07f298037569bd4de1
dwianiyulia <dwianiyulia@gmail.com>
2019-11-17 16:21:35 +0700

    ...

--- player.pl
+++ player.pl
@@ -7,46 +7,66 @@ inventory(snivy).
 posisiPlayer(1,2).
 posisiPokemon(1,1,2).
 jml_inventory(1).
+curr_health(snivy,10).
+curr_health(victini,100).
+curr_health(virizion,100).
 
 
 
 /* Inventory */
 /* add_inv(X,Y,Z) : menambahkan pokemon Y ke dalam list inventory X */
 add_inv([],X,[X]).
-add_inv([H|T],H,[H|T]).
 add_inv([Z|T1],X,[Z|T2]):- add_inv(T1,X,T2),!.
 
 /* del_inv(X,Y,Z) : menghapus pokemon Y dari inventory X */
-del_inv([X],X,[]).
-del_inv([Z|T1],X,[Z|T2]) :- del_inv(T1,X,T2),!.
+del_inv([H|T],H, T).
+del_inv([H, H2|T],X, [H|T2]) :-
+    del_inv([H2|T], X, T2),!.
+
+/* idx_inv(IN,X,IDX) : mengembalikan nilai indeks IDX elemen X dari inventori IN */
+idx_inv([H|T], H, 1).
+idx_inv([H|T], X, I) :-
+    idx_inv(T, X, Z),
+    I is (Z+1),!.
+
 
 /* catch(X) : Menangkap pokemon X yang sudah dikalahkan pada suatu lokasi tertentu dan memasukannya ke inventory I sehingga jumlah inventory bertambah 1 */
 capture(X) :-
     pokemon(Y,X),
     posisiPlayer(A,B),
     posisiPokemon(Y,A,B),
-    asserta(inventory(X)),
     jml_inventory(N),
     N1 is N+1,
     N1 < 7,
+    asserta(inventory(X)),
     retract(jml_inventory(N)),
     asserta(jml_inventory(N1)),
-    add_inv(I,X,I2),!.
+    update_inv,!.
+
 
 /* drop(X) : Menghapus pokemon X dari inventory */
 drop(X) :- 
     pokemon(Y,X),
+    inventory(X),
     retract(inventory(X)),
     jml_inventory(N),
     N1 is N-1,
     retract(jml_inventory(N)),
     asserta(jml_inventory(N1)),
-    del_inv(I,X,I2),!.
+    del_inv(I,X,I2),
+    update_inv,!.
+
+/* update_inv : mengupdate list inventory */
+update_inv :-
+    inventory(X),
+    add_inv([],X,List2).
+    idx_inv(List2,X,N2),
+    asserta(no_inventory(N2,X)),!.
 
 /* choose(X) : Pokemon X dipilih dari inventory untuk battle */
-pick(X) :- 
-    pokemon(Y,X),
-    findall(X,inventory(X),I2),!.
+pick(N) :- 
+    no_inventory(N,X),
+    pokemon(Y,X),!.
     /* buat attack (?) */
 
 /* gym center */
